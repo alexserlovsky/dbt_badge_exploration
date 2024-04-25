9b15ecfea4c851f5915414384215fe21abe0e211
Alex Serlovsky <alexserlovsky@gmail.com>
2024-04-25 03:59:01 +0000

    Setting up sources in yaml. Referencing using __source for better tracking in the lineage.

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,13 +1,33 @@
-with orders as (
+with
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }} 
+
+),
+
+staged as (
 
     select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
-
+    from source
 )
 
-select * from orders
+select * from staged
+
+-- with orders as (
+
+--     select
+--         id as order_id,
+--         user_id as customer_id,
+--         order_date,
+--         status
+
+--     from raw.jaffle_shop.orders
+
+-- )
+
+-- select * from orders
