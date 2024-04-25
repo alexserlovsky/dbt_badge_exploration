e2ace984fccc25a8a47b07ff56ff787e15823c2a
Alex Serlovsky <alexserlovsky@gmail.com>
2024-04-25 04:27:17 +0000

    Create a src_payments yaml.

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,3 +1,11 @@
+source as (
+
+    select * from {{ source('stripe', 'payment') }} 
+
+),
+
+staged as (
+
 select
     id as payment_id,
     orderid as order_id,
@@ -9,3 +17,19 @@ select
     created as created_at
 
 from raw.stripe.payment 
+
+)
+
+select * from staged
+
+-- select
+--     id as payment_id,
+--     orderid as order_id,
+--     paymentmethod as payment_method,
+--     status,
+
+--     -- amount is stored in cents, convert it to dollars
+--     amount / 100 as amount,
+--     created as created_at
+
+-- from raw.stripe.payment 
