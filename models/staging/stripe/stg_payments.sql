53a702834ad908006762eb62441f081802857a59
Alex Serlovsky <alexserlovsky@gmail.com>
2024-04-30 03:26:45 +0000

    src_payments is configured. stg_payments.sql is referencing src_payments.

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,35 +1,9 @@
-source as (
-
-    select * from {{ source('stripe', 'payment') }} 
-
-),
-
-staged as (
-
 select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-
-from raw.stripe.payment 
-
-)
-
-select * from staged
-
--- select
---     id as payment_id,
---     orderid as order_id,
---     paymentmethod as payment_method,
---     status,
-
---     -- amount is stored in cents, convert it to dollars
---     amount / 100 as amount,
---     created as created_at
-
--- from raw.stripe.payment 
+from {{ source('stripe', 'payment') }}
